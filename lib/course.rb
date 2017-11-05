f22b84ef3a57b844db9abb66b997d33b35e239a7
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-05 08:51:03 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,6 +1,12 @@
 class Course
   attr_accessor :title, :schedule, :description
   @@courses = []
+  def initialize(title,schedule,description)
+    @title = title
+    @schedule = schedule
+    @description = description
+    @@courses << self
+  end
   def self.all
     @@courses
   end
