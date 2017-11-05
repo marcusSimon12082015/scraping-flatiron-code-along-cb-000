b6f12333bdef3c95f519af54f2b6a3184b54170e
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-05 09:08:18 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+    @document
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -16,9 +16,9 @@ class Scraper
     end
   end
   def get_page
-      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+      @document = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+  def get_courses
+    @document.css(".post")
   end
 end
-
-
-
